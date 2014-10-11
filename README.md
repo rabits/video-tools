ab712cdef76231f01ddc2596cde3e9ec625e38d2
Rabits <home@rabits.org>
2014-10-11 04:05:27 +0400

    Initial commit
    * Added working script for linking gopro proxies

---
+++ README.md
@@ -0,0 +1,16 @@
+Kdenlive-Tools
+==============
+
+Contains scripts for the Kdenlive video editor:
+* gopro-proxy.py - can create proxies for kdenlive from gopro lrv files (tested on GoPro Hero 3+ Black)
+
+Requirements:
+-------------
+* python 2.7
+
+Support:
+--------
+If you like penguins or my hard work - you can support me by a small bitcoin donation :)
+
+My bitcoin wallet: `15phQNwkVs3fXxvxzBkhuhXA2xoKikPfUy`
+
---
+++ gopro-proxy.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+'''Kdenlive GoPro Proxy Linker 1.0
+
+Author:      Rabit <home@rabits.org>
+License:     GPL v3
+Description: This script will link your gopro LRV proxies to the kdenlive proxy files
+Required:    python2.7
+
+Usage:
+  1. Execute script in your project folder:
+    $ ./gopro-proxy.py <LRV_DIR> [ORIG_EXT=.MP4] [PROXY_EXT=.LRV] [PROXY_DIR=LRV_DIR/proxy]
+  2. Create new or use existing kdenlive project
+  3. Disable autocreation of proxies and change proxy ext to 'mp4' in your project settings
+  4. Select all gopro files and run proxy recreation
+'''
+
+import hashlib, os, sys
+
+LRV_DIR = sys.argv[1]
+
+ORIG_EXT = sys.argv[2] if len(sys.argv) > 2 else '.MP4'
+PROXY_EXT = sys.argv[3] if len(sys.argv) > 3 else '.LRV'
+PROXY_DIR = sys.argv[4] if len(sys.argv) > 4 else os.path.join(LRV_DIR, 'proxy')
+
+if not os.path.isdir(PROXY_DIR):
+    os.makedirs(PROXY_DIR)
+
+if not os.path.isdir(LRV_DIR):
+    print 'Error: wrong arguments'
+    exit(1)
+
+files = os.listdir(LRV_DIR)
+
+for f in files:
+    if f.lower().endswith(PROXY_EXT):
+        print 'Processing %s' % f
+        forig = f[:-len(PROXY_EXT)] + ORIG_EXT
+        if not forig in files:
+            print '  unable to find original file %s' % os.path.join(LRV_DIR, fname + ORIG_EXT)
+            exit(1)
+        m = hashlib.md5()
+        forig = os.path.join(LRV_DIR, forig)
+        fsize = os.path.getsize(forig)
+        with open(forig, "rb") as fd:
+            if fsize > 1000000*2:
+                m.update(fd.read(1000000))
+                fd.seek(fsize - 1000000)
+            m.update(fd.read())
+        link = os.path.join(PROXY_DIR, m.hexdigest()) + ORIG_EXT.lower()
+        target = os.path.relpath(os.path.join(LRV_DIR, f), PROXY_DIR)
+        print '  create link %s target: %s' % (link, target)
+        if os.path.lexists(link):
+            os.remove(link)
+        os.symlink(target, link)
