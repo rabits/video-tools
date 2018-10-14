ce8423212f8629a1b413501ddf17a9ff1ea6dc12
DefteZ <deftez@gmail.com>
2018-10-14 17:27:12 +0200

    Make python3 compatible

--- kdenlive-proxy-gopro.py
+++ kdenlive-proxy-gopro.py
@@ -5,7 +5,7 @@
 Author:      Rabit <home@rabits.org>
 License:     GPL v3
 Description: This script will link your gopro LRV proxies to the kdenlive proxy files
-Required:    python2.7
+Required:    python2.7 / python 3
 
 Usage:
   1. Execute script in your project folder:
@@ -27,17 +27,17 @@ if not os.path.isdir(PROXY_DIR):
     os.makedirs(PROXY_DIR)
 
 if not os.path.isdir(LRV_DIR):
-    print 'Error: wrong arguments'
+    print('Error: wrong arguments')
     exit(1)
 
 files = os.listdir(LRV_DIR)
 
 for f in files:
     if f.lower().endswith(PROXY_EXT.lower()):
-        print 'Processing %s' % f
+        print('Processing %s' % f)
         forig = f[:-len(PROXY_EXT)] + ORIG_EXT
         if not forig in files:
-            print '  unable to find original file %s' % os.path.join(LRV_DIR, forig)
+            print('  unable to find original file %s' % os.path.join(LRV_DIR, forig))
             exit(1)
         m = hashlib.md5()
         forig = os.path.join(LRV_DIR, forig)
@@ -49,7 +49,7 @@ for f in files:
             m.update(fd.read())
         link = os.path.join(PROXY_DIR, m.hexdigest()) + ORIG_EXT.lower()
         target = os.path.relpath(os.path.join(LRV_DIR, f), PROXY_DIR)
-        print '  create link %s target: %s' % (link, target)
+        print('  create link %s target: %s' % (link, target))
         if os.path.lexists(link):
             os.remove(link)
         os.symlink(target, link)
